00912b11c3dc290e0c82780854657dca6e32861f
Bruna Silveira <brunasilveira1@hotmail.com>
2022-08-30 19:15:59 -0300

    ajuste na lista

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
 document.getElementById('adicionar').addEventListener('click', function(){
-    const inputTarefa = document.getElementById('tarefa').value;
+    const inputTarefa = document.getElementById('tarefa');
     const tarefa = inputTarefa.value;
     if (tarefa.trim()){
         const li = document.createElement ('li');
